57d70470d6f9baee7954ee31558b68f19872bfd8
Divij Jain <68986989+diiviij@users.noreply.github.com>
2022-10-04 18:45:27 +0530

    Create Case4.py

---
+++ Case4.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+class LoginPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def login(self, username, password):
+        username_input = self.browser.find_element_by_css_selector("input[name='username']")
+        password_input = self.browser.find_element_by_css_selector("input[name='password']")
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button = browser.find_element_by_xpath("//button[@type='submit']")
+        login_button.click()
+        sleep(5)
+
+class HomePage:
+    def __init__(self, browser):
+        self.browser = browser
+        self.browser.get('https://www.instagram.com/')
+
+    def go_to_login_page(self):
+        self.browser.find_element_by_xpath("//a[text()='Log in']").click()
+        sleep(2)
+        return LoginPage(self.browser)
